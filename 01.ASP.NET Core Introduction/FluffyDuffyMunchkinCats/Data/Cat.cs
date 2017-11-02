a4d9d4a8d331577b2503722a62c977ac53e42d50
kdamyanov <kirildam@gmail.com>
2017-11-02 15:11:22 +0200

    Add FluffyDuffyMunchkinCats Project

---
+++ 01.ASP.NET Core Introduction/FluffyDuffyMunchkinCats/Data/Cat.cs
@@ -0,0 +1,26 @@
+﻿namespace FluffyDuffyMunchkinCats.Data
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class Cat
+    {
+        private const int StringMaxLenght = 50;
+
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(StringMaxLenght)]
+        public string Name { get; set; }
+
+        [Range(0, 30)]
+        public int Age { get; set; }
+
+        [Required]
+        [MaxLength(StringMaxLenght)]
+        public string Breed { get; set; }
+
+        [Required]
+        [MaxLength(2000)]
+        public string ImageUrl { get; set; }
+    }
+}
---
+++ 01.ASP.NET Core Introduction/FluffyDuffyMunchkinCats/Startup.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FluffyDuffyMunchkinCats
+{
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.Extensions.DependencyInjection;
+    using Data;
+
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<CatsDbContext>(options =>
+                options.UseSqlServer("Server=.;Database=CatsServerDb;Integrated Security=True;"));
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+            }
+
+            app.Run(async (context) =>
+            {
+                context.Response.StatusCode = 404;
+                await context.Response.WriteAsync("404 Page Was Not Found :/");
+            });
+        }
+    }
+}
